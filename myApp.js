54a4be23fa6d0c364b0a1df663da75281c67dcd7
zawmintun79710 <zawmintuncoe.mm@gmail.com>
2022-08-14 07:51:21 +0630

    Change syntax

--- myApp.js
+++ myApp.js
@@ -5,7 +5,7 @@ const {Schema} = mongoose;
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person = mongoose.model("personSchema", new Schema({
-name: String, age:Number, favouriteFoods:[String]
+name: String, age:Number, favoriteFoods:[String]
 }));
 
 const createAndSavePerson = (done) => {
