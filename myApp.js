73539f3a6dc6868fc9f3870c2fef467577cd1917
zawmintun79710 <zawmintuncoe.mm@gmail.com>
2022-08-14 20:28:34 +0630

    Change queryChain()

--- myApp.js
+++ myApp.js
@@ -77,8 +77,9 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+Person.find({favoriteFoods:foodToSearch}).sort("name").limit(2).select("name","favoriteFoods").exec((err, data)=>{
+  done(null , data);
+})
 };
 
 /** **Well Done !!**
