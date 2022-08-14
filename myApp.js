f31365ba261c072d2f31fe0ceee6083be12e943b
zawmintun79710 <zawmintuncoe.mm@gmail.com>
2022-08-14 07:48:52 +0630

    Create Schema and Model

--- myApp.js
+++ myApp.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 let mongoose = require('mongoose');
+const {Schema} = mongoose;
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-
-let Person;
+let Person = mongoose.model("personSchema", new Schema({
+name: String, age:Number, favouriteFoods:[String]
+}));
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
